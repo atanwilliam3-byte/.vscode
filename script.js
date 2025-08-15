e44d557ef98abf19560a50c89f07526c24500a51
atanwilliam3-byte <atanwilliam3@gmail.com>
2025-08-15 23:05:24 +0330

    Add files via upload
    html

---
+++ script.js
@@ -0,0 +1,15 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.top = "100vh";
+    heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+
+setInterval(createHeart, 300);
