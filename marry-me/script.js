f4354bc57b0e294ba9924691f303820508b38390
hoho <atanwilliam3@gmail.com>
2025-08-16 23:38:47 +0330

    Add files via upload

---
+++ marry-me/script.js
@@ -0,0 +1,6 @@
+ document.getElementById("yesBtn").onclick = function() {
+     document.getElementById("message").innerText="اوا خانوم میبینم  باهام ازدواج کردی💍😏";
+ };
+  document.getElementById("noBtn").onclick = function(){
+
+  };
