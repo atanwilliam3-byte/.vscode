3d04ed8999b6fba4788eae34ade259e631f3451a
hoho <atanwilliam3@gmail.com>
2025-08-16 23:27:41 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.getElementById("yesBtn").onclick = function() {
+     document.getElementById("message").innerText="مرسی بانو❤️🥳💍";
+ };
+  document.getElementById("noBtn").onclick = function(){
+
+  };
